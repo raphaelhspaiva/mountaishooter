63ea5009a4a78b448db7af0fb767d2046ee5d6ae
Raphael Paiva <raphaelhspaiva@outlook.com>
2026-06-19 15:33:37 -0300

    Initial commit

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() # Close Window
-            quit() # end pygame
+            quit() # end pygame.
 
