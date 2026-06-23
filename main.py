4d0643cf92f5441951a2b69769ffaf44715d650b
Raphael Paiva <raphaelhspaiva@outlook.com>
2026-06-23 11:08:42 -0300

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print('Loop Start')
 while True:
     # Check for all events
     for event in pygame.event.get():
-        print('Quinting...'
+        print('Quintting...'
         if event.type == pygame.QUIT:
             pygame.quit() # Close Window
             quit() # end pygame.
